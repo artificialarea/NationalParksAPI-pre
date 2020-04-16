56e8ab36defcc9c40bc5a5c1ca07194d133d250a
Sacha Sedriks <sachased@gmail.com>
2020-04-16 11:39:06 -0700

    prep'd html for jquery. added comments to js outlining requirements. updated js with function stubs and initial pseudocode.

--- script.js
+++ script.js
@@ -1,2 +1,91 @@
 'use strict';
 
+// GOAL: Display a list of national parks in an area
+
+// REQUIREMENTS /////////////////////////////////
+
+// The user must be able to search for parks in one or more states.
+// ^^^^^ PRESUMPTION: not simultaneously, so a dropdown to pick one state per submission is fine.
+
+// The user must be able to set the max number of results, with a default of 10.
+
+// The search must trigger a call to NPS's API.
+
+// The parks in the given state must be displayed on the page. Include at least:
+// Full name
+// Description
+// Website URL
+
+// The user must be able to make multiple searches and see only the results for the current search.
+
+// As a stretch goal, try adding the park's address to the results.
+
+// end REQUIREMENTS /////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////
+// SEPARATION OF CONCERNS: TYPES OF FUNCTIONS
+// (Miscellaneous): Fetch Data
+// Template Generators
+// Rendering Functions
+// Event Handlers
+//////////////////////////////////////////////////////////////
+
+// INIT
+function init() {
+  renderDropdownMenu();
+  handleSubmission();
+}
+
+// MISCELLANEOUS /////////////////////////////////////////////
+
+
+function fetchStateParkInfo() {
+  // const params
+  // const url
+  // get/fetch
+  // renderStateParkInfo();
+}
+
+function fetchListOfStates() {
+  // get list of available states to populated the dropdown menu
+}
+
+
+// TEMPLATE GENERATORS ///////////////////////////////////////
+
+function generateStateParkInfo() {
+  // extract .map all relevant key/values into array
+  // return as string
+}
+
+function generateDropdownMenu() {
+    // extract .map all relevant key/values into array
+    // return as string of <options>
+}
+
+
+// RENDERING FUNCTIONS ///////////////////////////////////////
+
+function renderStateParkInfo() {
+  // generate info into HTML
+  // render HTML into DOM
+
+}
+
+function renderDropdownMenu() {
+  // fetch list of states available
+  // generate list of <options>
+  // render HTML in the DOM
+}
+
+
+// EVENT HANDLERS ////////////////////////////////////////////
+
+function handleSubmission() {
+
+}
+
+
+// INVOKE INIT
+$(init);
